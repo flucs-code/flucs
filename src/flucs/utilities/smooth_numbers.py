6a805931b4b1ee0d087df5c4ad797fe447348394
plamen.ivanov257 <plamen.ivanov257@gmail.com>
2025-08-29 14:22:39 +0100

    strictly bigger smooth numbers

--- src/flucs/utilities/smooth_numbers.py
+++ src/flucs/utilities/smooth_numbers.py
@@ -2,7 +2,7 @@ import heapq
 
 
 def next_smooth_number(n: int, primes: list = None) -> int:
-    """Returns the smallest number that is bigger
+    """Returns the smallest number that is strictly bigger
     than a given number n and divisible only by
     the prime numbers specified in primes.
 
@@ -31,7 +31,7 @@ def next_smooth_number(n: int, primes: list = None) -> int:
     while True:
         guess = heapq.heappop(heap)
 
-        if n <= guess:
+        if n < guess:
             return guess
 
         for p in primes:
