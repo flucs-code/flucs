5829134d074197ae2b6bba9424c4bca1f861ec14
Plamen Ivanov <plamen.ivanov257@gmail.com>
2026-07-03 14:24:41 +0100

    restructuring

---
+++ src/flucs/solvers/fourier/timesteppers/ab3.py
@@ -0,0 +1,215 @@
+import cupy as cp
+import numpy as np
+
+from flucs.solvers import FlucsSolver, FlucsTimestepper
+from flucs.solvers.fourier.fourier_system import FourierSystem
+from flucs.utilities.cupy import KernelWrapper, cupy_set_device_pointer
+from flucs.utilities.messages import flucsprint
+
+
+class FourierAB3Timestepper(FlucsTimestepper[FourierSystem]):
+    lawson_integrating_factors: bool
+    is_nonlinear: bool
+
+    dt_array: np.ndarray
+    ab3_coefficients: np.ndarray
+
+    # CUDA kernels
+    precompute_iteration_matrices_kernel: KernelWrapper
+    finish_step_kernel: KernelWrapper
+
+    def _update_ab3_coefficients(self) -> None:
+        """
+        Updates nonlinear coefficients given changing timestep.
+        """
+        self.dt_array[self.system.current_step % 3] = self.system.current_dt
+
+        # Alias for readability
+        current_step = self.system.current_step
+        dt0 = self.dt_array[current_step % 3]
+        dt1 = self.dt_array[current_step % 3 - 1]
+        dt2 = self.dt_array[current_step % 3 - 2]
+
+        # Compute coefficients.
+        # Disabling formatting and linting for readability.
+        # fmt: off
+        self.ab3_coefficients[0] = 1 + (dt0 / dt1) * ((2.0 / 6.0) * dt0 +               dt1 + (3.0 / 6.0) * dt2) / (dt1 + dt2) # noqa: E501
+        self.ab3_coefficients[1] =   - (dt0 / dt1) * ((2.0 / 6.0) * dt0 + (3.0 / 6.0) * dt1 + (3.0 / 6.0) * dt2) / (      dt2) # noqa: E501
+        self.ab3_coefficients[2] =   + (dt0 / dt2) * ((2.0 / 6.0) * dt0 + (3.0 / 6.0) * dt1                    ) / (dt1 + dt2) # noqa: E501
+        # fmt: on
+
+    def setup(self):
+        if not self.input["timestepping.precompute_linear_matrix"]:
+            self.precompute_iteration_matrices = lambda: None
+
+        self._allocate_memory()
+
+    def ready(self):
+        system = self.system
+        self.dt_array = np.array(
+            [system.current_dt, 10**10, 10**10], dtype=system.float
+        )
+        self.ab3_coefficients = np.array([1, 0, 0], dtype=system.float)
+
+        # Reset AB3 history
+        if system.requires_explicit_terms:
+            self.multistep_explicit_terms.fill(system.complex(0.0))
+            cupy_set_device_pointer(
+                system.cupy_module,
+                "multistep_explicit_terms",
+                self.multistep_explicit_terms,
+            )
+
+        if self.input["timestepping.precompute_linear_matrix"]:
+            if self.lawson_integrating_factors:
+                cupy_set_device_pointer(
+                    system.cupy_module, "propagator_precomp", self.propagator
+                )
+            else:
+                cupy_set_device_pointer(
+                    system.cupy_module, "inverse_lhs_precomp", self.inverse_lhs
+                )
+                cupy_set_device_pointer(
+                    system.cupy_module, "rhs_precomp", self.rhs
+                )
+
+            self.precompute_iteration_matrices()
+
+    def _allocate_memory(self):
+        # For the explicit terms, we need to keep terms at the current
+        # time step + terms from the past 2 time steps since we are
+        # using AB3.
+        # The explicit terms are indexed as (step, field, kz, kx, ky)
+        system = self.system
+        if self.system.requires_explicit_terms:
+            self.multistep_explicit_terms = cp.zeros(
+                (
+                    3,
+                    system.number_of_fields,
+                    system.nz,
+                    system.nx,
+                    system.half_ny,
+                ),
+                dtype=system.complex,
+            )
+
+        # Allocate precomputation matrices
+        if self.input["timestepping.precompute_linear_matrix"]:
+            # Allocate according to method
+            if self.lawson_integrating_factors:
+                if not hasattr(self, "propagator"):
+                    self.propagator = cp.zeros(
+                        (
+                            system.number_of_fields,
+                            system.number_of_fields,
+                            system.nz,
+                            system.nx,
+                            system.half_ny,
+                        ),
+                        dtype=system.complex,
+                    )
+            else:
+                if not hasattr(self, "rhs"):
+                    self.rhs = cp.zeros(
+                        (
+                            system.number_of_fields,
+                            system.number_of_fields,
+                            system.nz,
+                            system.nx,
+                            system.half_ny,
+                        ),
+                        dtype=system.complex,
+                    )
+                    self.inverse_lhs = cp.zeros(
+                        (
+                            system.number_of_fields,
+                            system.number_of_fields,
+                            system.nz,
+                            system.nx,
+                            system.half_ny,
+                        ),
+                        dtype=system.complex,
+                    )
+
+
+    def precompute_iteration_matrices(self):
+        """Precomputes the linear matrix."""
+        self.precompute_iteration_matrices_kernel(
+            self.system.float(self.system.current_dt)
+        )
+
+    def setup_cuda_definitions(self):
+        if self.lawson_integrating_factors:
+            self.system.module_options.define_flag("AB3_IF")
+        else:
+            self.system.module_options.define_flag("AB3")
+
+        self.system.module_options.define_flag(
+            "LINEAR_PADE_DEGREE", str(
+                self.input["timestepping.linear_pade_degree"]
+            )
+        )
+
+        if self.input["timestepping.precompute_linear_matrix"]:
+            flucsprint("Linear matrices will be precomputed.", source=self)
+            self.system.module_options.define_flag("PRECOMPUTE_LINEAR_MATRIX")
+
+    def register_kernels(self) -> None:
+        """Registers the CUDA kernels."""
+        self.precompute_iteration_matrices_kernel = KernelWrapper(
+            system=self.system,
+            cuda_kernel_name="precompute_iteration_matrices",
+            grid=(self.system.half_unpadded_cuda_grid_size,),
+            block=(self.system.cuda_block_size,),
+        )
+
+        self.finish_step_kernel = KernelWrapper(
+            system=self.system,
+            cuda_kernel_name="finish_step",
+            grid=(self.system.half_unpadded_cuda_grid_size,),
+            block=(self.system.cuda_block_size,),
+        )
+
+    def execute_timestep(self):
+        system = self.system
+
+        if self.is_nonlinear:
+            fields = system.fields[
+                (system.current_step - 1) % system.fields_history_size
+            ]
+            system.compute_nonlinear_terms(fields)
+
+            if system._update_dt():
+                self.precompute_iteration_matrices()
+
+            self._update_ab3_coefficients()
+
+        self.finish_step_kernel(
+            system.float(system.current_dt),
+            system.float(system.current_time),
+            system.int(system.current_step),
+            system.float(system.adaptive_rate),
+            self.ab3_coefficients[0],
+            self.ab3_coefficients[1],
+            self.ab3_coefficients[2],
+            system.fields[system.current_step % system.fields_history_size - 1],
+            system.dft_bits,
+            system.fields[system.current_step % system.fields_history_size],
+        )
+
+    def __init__(self, solver: FlucsSolver[FourierSystem]):
+        super().__init__(solver)
+        self.lawson_integrating_factors = (
+            self.input["timestepping.lawson_integrating_factors"]
+        )
+
+        self.is_nonlinear = not self.system.input["setup.linear"]
+
+    def __str__(self):
+        if self.lawson_integrating_factors:
+            return (
+                "Adams-Bashforth 3 with "
+                "Lawson integrating factors (Pade approx.)"
+            )
+
+        return "Adams-Bashforth 3"
