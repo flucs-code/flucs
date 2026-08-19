d15b525820606d2a6517c115927deb74ea24b783
Liam Pattinson <liampattinson@gmail.com>
2026-08-19 12:22:34 +0100

    Expand FlucsInput tests

--- tests/test_input.py
+++ tests/test_input.py
@@ -1,3 +1,8 @@
+import re
+from textwrap import dedent
+
+import pytest
+
 from flucs.input import FlucsInput
 
 
@@ -14,7 +19,82 @@ def test_flucs_input(mock_input_path, mock_solver, mock_system):
     assert input["dimensions.ny"] == 64
     assert input["dimensions.nz"] == 1
 
+
+def test_create_solver_system(mock_input_path, mock_solver, mock_system):
+    input = FlucsInput(mock_input_path)
     # Test that the correct solver and system types are loaded
     solver, system = input.create_solver_system()
     assert solver == mock_solver.return_value
     assert system == mock_system.return_value
+
+
+def test_load_dict():
+    # Create an uninitialized instance
+    input = FlucsInput.__new__(FlucsInput)
+    input._input_dict = {}
+    input._default_input_dict = {}
+
+    default_dict = {
+        "parameters": {"alpha": 1.0, "beta": 1.0},
+        "dimensions": {"nx": 1, "ny": 1},
+    }
+
+    # Test with default=False
+    with pytest.raises(ValueError) as excinfo:
+        input.load_dict(default_dict)
+    assert re.search(r"Parameter '[a-z]*' is invalid", str(excinfo.value))
+
+    # Test with default=True
+    input.load_dict(default_dict, default=True)
+    assert input["parameters"] == {"alpha": 1.0, "beta": 1.0}
+    assert input["dimensions"] == {"nx": 1, "ny": 1}
+
+    # Test with default=False, which should override the defaults
+    new_dict = {
+        "parameters": {"alpha": 2.0, "beta": 3.0},
+        "dimensions": {"nx": 4, "ny": 5},
+    }
+    input.load_dict(new_dict)
+    assert input["parameters"] == {"alpha": 2.0, "beta": 3.0}
+    assert input["dimensions"] == {"nx": 4, "ny": 5}
+
+
+def test_load_toml_str():
+    # Create an uninitialized instance
+    input = FlucsInput.__new__(FlucsInput)
+    input._input_dict = {}
+    input._default_input_dict = {}
+
+    default_toml = dedent("""\
+        [parameters]
+        alpha = 1.0
+        beta = 1.0
+
+        [dimensions]
+        nx = 1
+        ny = 1
+    """)
+
+    # Test with default=False
+    with pytest.raises(ValueError) as excinfo:
+        input.load_toml_str(default_toml)
+    assert re.search(r"Parameter '[a-z]*' is invalid", str(excinfo.value))
+
+    # Test with default=True
+    input.load_toml_str(default_toml, default=True)
+    assert input["parameters"] == {"alpha": 1.0, "beta": 1.0}
+    assert input["dimensions"] == {"nx": 1, "ny": 1}
+
+    # Test with default=False, which should override the defaults
+    new_toml = dedent("""\
+        [parameters]
+        alpha = 2.0
+        beta = 3.0
+
+        [dimensions]
+        nx = 4
+        ny = 5
+    """)
+    input.load_toml_str(new_toml)
+    assert input["parameters"] == {"alpha": 2.0, "beta": 3.0}
+    assert input["dimensions"] == {"nx": 4, "ny": 5}
