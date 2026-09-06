17ee0e32c50df69b2a7e90667c9008afcb5d2bc5
plamen.ivanov257 <plamen.ivanov257@gmail.com>
2026-09-06 15:22:20 +0100

    minor wording change

--- src/flucs/restart.py
+++ src/flucs/restart.py
@@ -222,8 +222,8 @@ class FlucsRestart:
         ):
             raise InvalidFlucsInputFileError(
                 "You must remove existing 'restart.nc' manually if "
-                "write_restart_file is 'True' but both restart_if_exists "
-                "and restart_from are 'False'."
+                "write_restart_file is 'True' but restart_if_exists is 'False' "
+                "and restart_from is not specified."
             )
 
     def write_restart(self, force: bool = False) -> None:
